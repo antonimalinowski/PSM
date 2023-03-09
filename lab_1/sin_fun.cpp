e9a924e6ebf3d902b9143687a062130847f72e4e
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-09 13:41:43 +0100

    Add custom factorial method, calulcate sin function, TO BE DONE - DEBUG

--- lab_1/sin_fun.cpp
+++ lab_1/sin_fun.cpp
@@ -1,15 +1,20 @@
 ﻿#include <iostream>
+#include <math.h>
 using namespace std;
 
+int precision = 16;
 double input_to_radians(string input);
+long fact(int num);
 
 int main()
 {
     string input;
     cout << "Enter the argument (in degrees) for sin function: ";
     cin >> input;
-    double res = input_to_radians(input);
-    cout << "Argument in radians is: " << res;
+    double radians = input_to_radians(input);
+    double res = sin(radians);
+    cout << "Argument in radians is: " << radians << endl;
+    cout << "Sin(x) for your input: " << res << endl;
     return 0;
 }
 
@@ -18,4 +23,30 @@ double input_to_radians(string input)
     int degrees = stod(input);
     double pi = 3.14159265359;
     return (degrees * (pi / 180));
+}
+
+double sin(double radians)
+{
+    double res = 0;
+    for (int i = 0; i < precision; i++)
+    {
+        int sign = pow(-1, i);
+        double nominator = pow(radians, i + 1);
+        double denominator = fact(i + 1);
+        res = res + sign * nominator / denominator;
+        i++;
+    }
+
+    return res;
+}
+
+long fact(int num)
+{
+    long factorial = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        factorial *= i;
+    }
+
+    return factorial;
 }

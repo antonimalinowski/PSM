97f783dcac9ca886ea58767cad0b0f963b8fc5bf
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-09 11:11:38 +0100

    Prompt user for the argument and convert it to radians, update README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ﻿This repo consists of notes and project done during the PSM (Introduction to Computer Simulation) course at PJAIT.
+
+To compile and run c++ programs:
+g++ -o file file.cpp && file
---
+++ lab_1/sin_fun.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+double input_to_radians(string input);
+
+int main()
+{
+    string input;
+    cout << "Enter the argument (in degrees) for sin function: ";
+    cin >> input;
+    double res = input_to_radians(input);
+    cout << "Argument in radians is: " << res;
+    return 0;
+}
+
+double input_to_radians(string input)
+{
+    int degrees = stod(input);
+    double pi = 3.14159265359;
+    return (degrees * (pi / 180));
+}
